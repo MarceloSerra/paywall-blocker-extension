a1154119f1fff04a928c88cc85cd0c5884868471
Marcelo Serra <celo_job@live.com>
2023-03-12 19:48:24 -0300

    feat: initial setup

---
+++ public/README.md
@@ -0,0 +1,15 @@
+## Paywall Blocker Extension
+
+- Just an extension to block paywall requests from some brazilian news sites
+- Status: WIP
+
+## Setup
+
+- In **Google Chrome** go to Options > More Tools > Extensions
+- Enable the **Developer Mode** option
+- Click on **Load Unpacked** and find the public folder of the project
+- Enable the extension
+
+## Using
+
+- Open some _Estadao_ news and check if the extension works
---
+++ public/app/background.js
@@ -0,0 +1,27 @@
+const urlsToBlock = {
+	estadao: [
+		"https://acesso.estadao.com.br/paywall/v2/config.php",
+		"https://www.estadao.com.br/zephr/features",
+		"https://www.estadao.com.br/zephr/feature-decisions",
+	],
+	// TODO: Add more domains
+};
+
+const mergeUrlsToBlock = (urls) => {
+	return Object.entries(urls).reduce((acc, [_domain, urls]) => {
+		return (acc = [...acc, ...urls]);
+	}, []);
+};
+
+const blockRequests = (details) => {
+	chrome.webRequest.onBeforeSendHeaders.addListener(
+		() => {
+			console.log({ background: details });
+			return { cancel: true };
+		},
+		{ urls: [mergeUrlsToBlock(urlsToBlock)] },
+		["blocking", "requestHeaders"]
+	);
+};
+
+blockRequests();
